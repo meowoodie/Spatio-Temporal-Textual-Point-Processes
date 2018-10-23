6f24db19329cbad16c36bdd4c90edfd25e5fa9b2
meowoodie <woodielove@163.com>
2018-10-22 22:34:24 -0400

    for testing spatial params

--- mppem.py
+++ mppem.py
@@ -219,8 +219,8 @@ if __name__ == '__main__':
     # np.set_printoptions(suppress=True)
     epoches  = 5
     iters    = 1
-    category = 'burglary'
-    t, m, l, u, u_set, specific_labels = utils.load_police_training_data(n=350, category=category)
+    category = 'other'
+    t, m, l, u, u_set, specific_labels = utils.load_police_training_data(n=10056, category=category)
 
     init_precision  = 0
     init_recall     = 0
@@ -230,12 +230,25 @@ if __name__ == '__main__':
     init_em = MPPEM(seq_t=t, seq_u=u, seq_l=l, seq_m=m, d=len(u_set), beta_1=1., beta_2=1.)
     init_em.init_Mu(alpha=1e+2)
 
-    for beta in np.linspace(-5, 10, 51):
+    # t = t[0:50] + t[3700:3900] + t[10000, 10056]
+    # u = u[0:50] + u[3700:3900] + u[10000, 10056]
+    # l = l[0:50] + l[3700:3900] + l[10000, 10056]
+    # m = m[0:50] + m[3700:3900] + m[10000, 10056]
+    burglary_with_random_indice   = list(range(0, 50))
+    pedrobbery_with_random_indice = list(range(3700, 3900))
+    others_with_random_indice     = list(range(10000, 10056))
+    indice = burglary_with_random_indice + pedrobbery_with_random_indice + others_with_random_indice
+    t  = np.array([ t[idx] for idx in indice ])
+    u  = np.array([ u[idx] for idx in indice ])
+    m  = np.array([ m[idx] for idx in indice ])
+    l  = [ l[idx] for idx in indice ]
+
+    for beta in np.linspace(-5, 5, 11):
         precision = []
         recall    = []
         print('---------beta = 10^%f ----------' % beta)
         for e in range(epoches):
-            em = MPPEM(seq_t=t, seq_u=u, seq_l=l, seq_m=m, d=len(u_set), beta_1=1., beta_2=10**beta)
+            em = MPPEM(seq_t=t, seq_u=u, seq_l=l, seq_m=m, d=len(u_set), beta_1=beta, beta_2=10**2)
             em.Mu = init_em.Mu
             init_p, init_r, p, r = em.fit(T=t[-1], tau=t[0], epoches=iters, first_N=500, specific_labels=specific_labels)
             precision.append(p)
@@ -243,13 +256,11 @@ if __name__ == '__main__':
         precisions.append(precision)
         recalls.append(recall)
 
-    np.savetxt("result/%s_precision_beta2_from-5to10.txt" % category, precisions, delimiter=',')
-    np.savetxt("result/%s_recalls_beta2_from-5to10.txt" % category, recalls, delimiter=',')
+    np.savetxt("result/%s_precision_beta1_from-5to10.txt" % category, precisions, delimiter=',')
+    np.savetxt("result/%s_recalls_beta1_from-5to10.txt" % category, recalls, delimiter=',')
 
     print(init_p)
     print(init_r)
-
-
 
     category = 'robbery'
     t, m, l, u, u_set, specific_labels = utils.load_police_training_data(n=350, category=category)
@@ -259,15 +270,12 @@ if __name__ == '__main__':
     precisions      = []
     recalls         = []
 
-    init_em = MPPEM(seq_t=t, seq_u=u, seq_l=l, seq_m=m, d=len(u_set), beta_1=1., beta_2=1.)
-    init_em.init_Mu(alpha=1e+2)
-
-    for beta in np.linspace(-5, 10, 51):
+    for beta in np.linspace(-10, 10, 11):
         precision = []
         recall    = []
         print('---------beta = 10^%f ----------' % beta)
         for e in range(epoches):
-            em = MPPEM(seq_t=t, seq_u=u, seq_l=l, seq_m=m, d=len(u_set), beta_1=1., beta_2=10**beta)
+            em = MPPEM(seq_t=t, seq_u=u, seq_l=l, seq_m=m, d=len(u_set), beta_1=beta, beta_2=10**2)
             em.Mu = init_em.Mu
             init_p, init_r, p, r = em.fit(T=t[-1], tau=t[0], epoches=iters, first_N=500, specific_labels=specific_labels)
             precision.append(p)
@@ -275,8 +283,8 @@ if __name__ == '__main__':
         precisions.append(precision)
         recalls.append(recall)
 
-    np.savetxt("result/%s_precision_beta2_from-5to10.txt" % category, precisions, delimiter=',')
-    np.savetxt("result/%s_recalls_beta2_from-5to10.txt" % category, recalls, delimiter=',')
+    np.savetxt("result/%s_precision_beta1_from-5to10.txt" % category, precisions, delimiter=',')
+    np.savetxt("result/%s_recalls_beta1_from-5to10.txt" % category, recalls, delimiter=',')
 
     print(init_p)
     print(init_r)
