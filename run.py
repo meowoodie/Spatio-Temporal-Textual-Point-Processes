68f115146cb68e4708a60fcec0d739be05d3e1ca
meowoodie <woodielove@163.com>
2018-10-30 23:02:17 -0400

    test retrieval

--- run.py
+++ run.py
@@ -119,6 +119,48 @@ def exp_alpha(
     np.savetxt("result/%s_precision_alpha_from%dto%d.txt" % (category, min(alpha_range), max(alpha_range)), precisions, delimiter=',')
     np.savetxt("result/%s_recalls_alpha_from%dto%d.txt" % (category, min(alpha_range), max(alpha_range)), recalls, delimiter=',')
 
+def exp_retrival(
+        retrieval_range=np.linspace(100, 1000, 51).astype(np.int32), n=10056,
+        category='other', epoches=5, iters=5,
+        csv_filename='data/beats_graph.csv'):
+    # load dataset
+    t, _, m, l, u, u_set, specific_labels = utils.load_police_training_data(n=n, category=category)
+    # only select a small set of data for category other
+    if category == 'other':
+        t  = np.array([ t[idx] for idx in indice ])
+        u  = np.array([ u[idx] for idx in indice ])
+        m  = np.array([ m[idx] for idx in indice ])
+        l  = [ l[idx] for idx in indice ]
+    # init results
+    precisions = []
+    recalls    = []
+
+    # in order to save time, do the initialization of mu one-time at first
+    init_em = MPPEM(seq_t=t, seq_u=u, seq_l=l, seq_m=m, d=len(u_set))
+    # init A
+    distance_matrix = utils.calculate_beats_pairwise_distance(u_set, csv_filename)
+    init_em.init_A(distance_matrix, gamma=1.)
+    # init Mu
+    init_em.init_Mu(gamma=1.)
+
+    # experiments
+    for N in retrieval_range:
+        precision = []
+        recall    = []
+        print('---------N = %d ----------' % N)
+        for e in range(epoches):
+            em = MPPEM(seq_t=t, seq_u=u, seq_l=l, seq_m=m, d=len(u_set), beta=1e+2, alpha=2.)
+            em.Mu = init_em.Mu
+            em.A  = init_em.A
+            ps, rs, _, _ = em.fit(T=t[-1], tau=t[0], iters=iters, first_N=N, specific_labels=specific_labels)
+            precision.append(ps[-1])
+            recall.append(rs[-1])
+        precisions.append(precision)
+        recalls.append(recall)
+    # save exp results
+    np.savetxt("result/%s_precision_N_from%dto%d.txt" % (category, min(retrieval_range), max(retrieval_range)), precisions, delimiter=',')
+    np.savetxt("result/%s_recalls_N_from%dto%d.txt" % (category, min(retrieval_range), max(retrieval_range)), recalls, delimiter=',')
+
 
 
 if __name__ == '__main__':
@@ -139,3 +181,12 @@ if __name__ == '__main__':
     #     category='other', epoches=3, iters=1)
 
     # exp_convergence(beta_1=1., beta_2=1e+2, alpha=1e+2, category='burglary', epoches=1, iters=25, n=350)
+    exp_retrival(
+        retrieval_range=np.linspace(100, 1000, 51).astype(np.int32), n=10056
+        category='robbery', epoches=3, iters=2)
+    exp_retrival(
+        retrieval_range=np.linspace(100, 1000, 51).astype(np.int32), n=350
+        category='burglary', epoches=3, iters=2)
+    exp_retrival(
+        retrieval_range=np.linspace(100, 1000, 51).astype(np.int32), n=10056
+        category='other', epoches=3, iters=2)
