fb36dc1d9adedecd811acbdda069db3e7d93b804
meowoodie <woodielove@163.com>
2019-12-15 21:14:32 -0500

    finish first version of new em algorithm

--- mppem.py
+++ mppem.py
@@ -15,27 +15,6 @@ class VecMarkedMultivarHawkes(object):
     Reference: https://arxiv.org/pdf/1902.00440.pdf
     '''
 
-    # def __init__(self, d, seq_t, seq_u, seq_l, seq_m=None, beta=1.):
-    #     # training data
-    #     self.t      = seq_t # time of each of events
-    #     self.u      = seq_u # component of each of events
-    #     self.m      = seq_m # marks (feature vectors) of each of events
-    #     self.l      = seq_l # labels of each of events
-    #     # basic configuration
-    #     self.T0     = self.t[0]
-    #     self.Tn     = self.t[-1]
-    #     self.n      = len(self.t) # number of events
-    #     self.d      = d           # number of components
-    #     # parameters for intensity kernel
-    #     self.beta   = beta                             # parameter for intensity kernel
-    #     self.A      = np.zeros((self.d, self.d))       # influential matrix for intensity kernel
-    #     self.A_mask = np.ones((self.d, self.d))        # mask for influential matrix
-    #     self.Mu     = np.random.uniform(0, 1, self.d)  # background rates for intensity kernel
-    #     self.P      = np.ones((self.n, self.n)) * -1   # transition probability matrix
-    #                                                    # -1 means uninitiated value
-    #     # normalization
-    #     self.t      = (self.t - self.T0) / (self.Tn - self.T0)
-
     def __init__(self, n_dim, T, seq, beta=1.):
         '''
         Params:
@@ -45,16 +24,18 @@ class VecMarkedMultivarHawkes(object):
                    the last dimension indicates time, dimension and mark, respectively.
         - beta:    (optional) temporal decaying factor.
         '''
-        self.seq   = seq
-        self.T     = T
-        self.n_dim = n_dim
-        self.beta  = beta
         # configuration
-        n_point = self.seq.shape[0]     # the number of points in the sequence
-        len_vec = self.seq.shape[1] - 2 # the length of the mark vector
-        self.P  = self._random_init_P(n_point)
+        self.seq     = seq
+        self.T       = T
+        self.n_dim   = n_dim
+        self.beta    = beta
+        self.n_point = self.seq.shape[0]     # the number of points in the sequence
+        self.len_vec = self.seq.shape[1] - 2 # the length of the mark vector
+        # model initialization
+        self.P  = self._random_init_P(self.n_point)
         self.A  = self._random_init_A(self.n_dim)
         self.Mu = self._prop_init_Mu(self.n_dim, self.seq, self.T)
+        self.M  = self._calculate_pairwise_mark_inner_prod(self.seq[:, 2:])
 
     @staticmethod
     def _random_init_P(n_point):
@@ -96,195 +77,174 @@ class VecMarkedMultivarHawkes(object):
             Mu[s] = c / T
         return Mu
 
+    @staticmethod
+    def _calculate_pairwise_mark_inner_prod(seq_m):
+        '''
+        Calculate matrix M that stores the inner product between m_i and m_j for 
+        all i and j. 
+        '''
+        n_point = seq_m.shape[0]
+        M       = np.zeros((n_point, n_point))
+        for i in range(n_point):
+            for j in range(i+1):
+                M[i, j] = M[j, i] = np.dot(seq_m[i, :], seq_m[j, :])
+        return M
+
     def _h(self, i, j):
         '''
         h_{ij} denotes a special term: 
-            A_{s_i, s_j} * beta * exp(- beta * (t_i - t_j)) h_i' * h_j.
+            h_{ij} = A_{s_i, s_j} * beta * exp(- beta * (t_i - t_j)) h_i' * h_j.
         This term will be further used in P matrix update. 
 
         See the definition of h_{ij} at Appendix D in the reference.
         '''
-        h_ij = self.A[self.seq[i, 1], self.seq[j, 1]] * \
+        h_ij = self.A[int(self.seq[i, 1]), int(self.seq[j, 1])] * \
                self.beta * np.exp(- self.beta * (self.seq[i, 0] - self.seq[j, 0])) * \
-               np.dot(self.seq[i, 2:], self.seq[j, 2:])
+               self.M[i, j]
         return h_ij
     
+    # Intermediate vectors which will be used in updating parameters
+
+    def _update_sum_h(self):
+        '''
+        Update vector _H that stores the sum of h_{ij} from j=1 to j=i-1, for all i.
+              _H_{ij}
+            = sum_{j=1}^{i-1} h_{ij}
+            = sum_{j=1}^{i-1} A_{s_i, s_j} * beta * exp(- beta * (t_i - t_j)) h_i' * h_j.
+        This term will be further used in P matrix update.
+        '''
+        self._H = np.zeros(self.n_point)
+        for i in range(self.n_point):
+            _h_i = [ self._h(i, j) for j in range(i) ]
+            self._H[i] = sum(_h_i)
+    
+    def _calculate_alpha_denominator(self):
+        '''
+        Calculate all possible the first term of the denominator of alpha_{uv}. 
+            1 - exp(- beta * (T - t_j))
+        This term will be further used in A matrix update
+        '''
+        self._DA = np.zeros(self.n_point)
+        for j in range(self.n_point):
+            self._DA[j] = 1 - np.exp(- self.beta * (self.T - self.seq[j, 0]))
+                
     def _update_P(self, i, j):
         '''
-        Update P_{ij} using an EM-like algorithm by maximizing the lower bound 
-        of the log-likelihood function. 
+        Update P_{ij} using an EM-like algorithm by maximizing the lower bound of the 
+        log-likelihood function w.r.t. P
         '''
         if i == j:
-            self.P[i, i] = self.Mu[i] / self. 
+            self.P[i, i] = self.Mu[int(self.seq[i, 1])] / (self.Mu[int(self.seq[i, 1])] + self._H[i])
+        elif j < i: 
+            self.P[i, j] = self._h(i, j) / (self.Mu[int(self.seq[i, 1])] + self._H[i])
 
-    # def _slide_window_indices(self, T, tau):
-    #     '''select the indices of the sequence within the slide window'''
-    #     return np.where((self.t < T) & (self.t >= tau))[0]
+    def _update_A(self, u, v):
+        '''
+        Update A_{uv} using an EM-like algorithm by maximizing the lower bound of the 
+        log-likelihood function w.r.t. A
+        of 
+        '''
+        seq_s = self.seq[:, 1]
+        # indicator of s_j == v and s_i == u
+        ind_sj_v = (seq_s.astype(np.int32) == v).astype(np.float32)
+        ind_si_u = (seq_s.astype(np.int32) == u).astype(np.float32)
+        denominator  = (ind_sj_v * self._DA * self.M.sum(axis=0)).sum()
+        numerator    = (np.outer(ind_si_u, ind_sj_v) * self.P).sum()
+        self.A[u, v] = numerator / denominator
 
-    # def _loglik_subterm_1(self, i, t_indices):
-    #     '''subterm 1 in log-likelihood function'''
-    #     terms = [
-    #         self.A[self.u[i]][self.u[j]] * 1. * np.exp(-1 * self.beta * (self.t[i] - self.t[j]) + np.inner(self.m[i], self.m[j]))
-    #         for j in t_indices[t_indices<i] ]
-    #     return np.array(terms)
+    # def retrieval_test(self, t_indices, specific_labels=None, first_N=100):
+    #     '''get precision and recall of retrieval test'''
+    #     # only do the test on the specific labels if specific_labels is not None
+    #     if specific_labels:
+    #         specific_label_cond = lambda i: self.l[i] in specific_labels
+    #     else:
+    #         specific_label_cond = lambda i: True
+    #     # get all the valid pairs
+    #     pairs = [ [ self.P[i][j], i, j ] for i in t_indices for j in range(i) ]
+    #     pairs = np.array(pairs)
+    #     pairs = pairs[pairs[:, 0].argsort()]
+    #     # print(len(pairs))
+    #     # get retrieve, hits and relevant
+    #     retrieve  = pairs[-first_N:, [1, 2]].astype(np.int32)
+    #     hits      = [ (i, j) for i, j in retrieve if self.l[i] == self.l[j] and specific_label_cond(i) ]
+    #     relevant  = [ (i, j) for i in t_indices for j in range(i) if self.l[i] == self.l[j] and specific_label_cond(i) ]
+    #     # print(len(relevant))
+    #     # get precision and recall
+    #     precision = len(hits) / len(retrieve) if len(retrieve) != 0 else 0.
+    #     recall    = len(hits) / len(relevant) if len(relevant) != 0 else 0.
+    #     return len(retrieve), precision, recall
 
-    # def _loglik_subterm_2(self, uj, t_indices, T):
-    #     '''subterm 2 in log-likelihood function'''
-    #     terms = [
-    #         self.A[self.u[i]][uj] * (1 - np.exp(- self.beta * (T - self.t[i])) + np.inner(self.m[i], self.m[t_indices[-1]]))
-    #         for i in t_indices]
-    #     return np.array(terms)
-
-    def log_likelihood(self, T, tau):
-        '''log-likelihood function given t (time sequence) and u (component sequence)'''
-        # get time indices of the indicated window
-        t_indices = self._slide_window_indices(T, tau)
-        term_1 = [
-            np.log(self.Mu[self.u[i]] + self._loglik_subterm_1(i, t_indices).sum())
-            for i in t_indices ]
-        term_2 = [ self.Mu[uj] * (T - tau) for uj in range(self.d) ]
-        term_3 = [ self._loglik_subterm_2(uj, t_indices, T).sum() for uj in range(self.d)]
-        loglik = sum(term_1) + sum(term_2) + sum(term_3)
-        return loglik
-
-    def jensens_lower_bound(self, T, tau):
-        '''lower bound for the log-likelihood function according to the jensen's inequality'''
-        # get time indices of the indicated window
-        t_indices        = self._slide_window_indices(T, tau)
-        t_indices_before = lambda i: t_indices[t_indices<i]
-        term_1 = [
-            self.P[i][i] * np.log(self.Mu[self.u[i]]) + \
-            (self.P[i][t_indices_before(i)] * np.log(self._loglik_subterm_1(i, t_indices) + 1e-100)).sum() - \
-            (self.P[i][t_indices_before(i)] * np.log(self.P[i][t_indices_before(i)] + 1e-100)).sum()
-            for i in t_indices ]
-        term_2 = [ self.Mu[uj] * (T - tau) for uj in range(self.d) ]
-        term_3 = [ self._loglik_subterm_2(uj, t_indices, T).sum() for uj in range(self.d)]
-        lowerb = sum(term_1) + sum(term_2) + sum(term_3)
-        return lowerb
-
-    def _update_P(self, i, j, T, tau):
-        '''update transition probability matrix P'''
-        # get time indices of the indicated window
-        t_indices = self._slide_window_indices(T, tau)
-        if i > j and j in t_indices and i in t_indices:
-            numerator    = (self.A[self.u[i]][self.u[j]]) * 1. * np.exp(-1 * self.beta * (self.t[i] - self.t[j]) + np.inner(self.m[i], self.m[j]))
-            denominator  = self.Mu[self.u[i]] + self._loglik_subterm_1(i, t_indices).sum()
-            self.P[i][j] = numerator / denominator
-
-    def _update_A(self, u, v, T, tau):
-        '''update influential matrix A'''
-        # get time indices of the indicated window
-        t_indices = self._slide_window_indices(T, tau)
-        # check if A_{u,v} is available
-        if self.A_mask[u][v]:
-            numerator = []
-            for i in t_indices[np.where(self.u[t_indices] == u)[0]]:
-                for j in t_indices[np.where(self.u[t_indices] == v)[0]]:
-                    if j < i:
-                        numerator.append(self.P[i][j])
-            denominator = [ 1 - np.exp(- self.beta * (T - self.t[j]))
-                for j in t_indices if self.u[j] == v ]
-            if len(numerator) == 0 or len(denominator) == 0:
-                self.A_mask[u][v] = 0
-                return
-            numerator    = sum(numerator)
-            denominator  = sum(denominator)
-            self.A[u][v] = (numerator / denominator)
-            # print('A(%d, %d) = %f' % (u, v, self.A[u][v]))
-
-    def retrieval_test(self, t_indices, specific_labels=None, first_N=100):
-        '''get precision and recall of retrieval test'''
-        # only do the test on the specific labels if specific_labels is not None
-        if specific_labels:
-            specific_label_cond = lambda i: self.l[i] in specific_labels
-        else:
-            specific_label_cond = lambda i: True
-        # get all the valid pairs
-        pairs = [ [ self.P[i][j], i, j ] for i in t_indices for j in range(i) ]
-        pairs = np.array(pairs)
-        pairs = pairs[pairs[:, 0].argsort()]
-        # print(len(pairs))
-        # get retrieve, hits and relevant
-        retrieve  = pairs[-first_N:, [1, 2]].astype(np.int32)
-        hits      = [ (i, j) for i, j in retrieve if self.l[i] == self.l[j] and specific_label_cond(i) ]
-        relevant  = [ (i, j) for i in t_indices for j in range(i) if self.l[i] == self.l[j] and specific_label_cond(i) ]
-        # print(len(relevant))
-        # get precision and recall
-        precision = len(hits) / len(retrieve) if len(retrieve) != 0 else 0.
-        recall    = len(hits) / len(relevant) if len(relevant) != 0 else 0.
-        return len(retrieve), precision, recall
-
-    def fit(self, T, tau, iters=100, first_N=100, specific_labels=None):
+    def em_fit(self, iters=100):
         '''
         maximize the lower bound of the loglikelihood function by estimating
         matrix P and matrix A iteratively.
         '''
-        # F-1 score
-        F_1 = lambda p, r: 2 * p * r / (p + r) if (p + r) != 0. else 0.
-        # normalization
-        T   = (T - self.T0) / (self.Tn - self.T0)
-        tau = (tau - self.T0) / (self.Tn - self.T0)
-        # get time indices of the indicated window
-        t_indices = self._slide_window_indices(T, tau)
-        print('[%s] %d points will be fitted.' % (arrow.now(), len(t_indices)))
-        # init P
-        self._init_P(t_indices)
+        # # F-1 score
+        # F_1 = lambda p, r: 2 * p * r / (p + r) if (p + r) != 0. else 0.
+        # # normalization
+        # T   = (T - self.T0) / (self.Tn - self.T0)
+        # tau = (tau - self.T0) / (self.Tn - self.T0)
+        # # get time indices of the indicated window
+        # t_indices = self._slide_window_indices(T, tau)
+        # print('[%s] %d points will be fitted.' % (arrow.now(), len(t_indices)))
+        # # init P
+        # self._init_P(t_indices)
         # n_alerts, init_precision, init_recall = self.retrieval_test(t_indices, specific_labels=specific_labels, first_N=first_N)
         # print('[%s] iter %d\tlower bound:\t%f' % (arrow.now(), 0, self.log_likelihood(T, tau)))
         # print('[%s] \t\tnum of alerts:%d,\tprecision:\t%f,\trecall:\t%f,\tF-1 score:\t%f.' % \
         #     (arrow.now(), n_alerts, init_precision, init_recall, F_1(init_precision, init_recall)))
-        # training iters
-        precisions = []
-        recalls    = []
-        logliks    = []
-        lowerbs    = []
+        # # training iters
+        # precisions = []
+        # recalls    = []
+        # logliks    = []
+        # lowerbs    = []
+
+        # calculate the denominator of A_{uv}
+        self._calculate_alpha_denominator()
         for e in range(iters):
-            # update P matrix:
-            for i in t_indices:
-                for j in t_indices[t_indices<i]:
-                    self._update_P(i, j, T, tau)
-            # update A matrix:
-            for u in range(self.d):
-                for v in range(self.d):
-                    self._update_A(u, v, T, tau)
-            # check sum of P
-            self.check_P(t_indices)
-            # get retrieval test results
-            n_alerts, precision, recall = self.retrieval_test(t_indices, specific_labels=specific_labels, first_N=first_N)
-            loglik = self.log_likelihood(T, tau)
-            lowerb = self.jensens_lower_bound(T, tau)
-            # logging
-            print('[%s] iter %d\tlog likli: %f,\tlower bound: %f' % (arrow.now(), e+1, loglik, lowerb))
-            print('[%s] \t\tnum of alerts: %d,\tprecision: %f,\trecall: %f,\tF-1 score: %f.' % \
-                (arrow.now(), n_alerts, precision, recall, F_1(precision, recall)))
-            precisions.append(precision)
-            recalls.append(recall)
-            logliks.append(loglik)
-            lowerbs.append(lowerb)
-        return precisions, recalls, logliks, lowerbs
+            print('[%s] iter %d' % (arrow.now(), e))
+            # update the denominator of P_{ij}
+            self._update_sum_h()
+            # update P matrix
+            for i in range(self.n_point):
+                for j in range(i+1):
+                    self._update_P(i, j)
+            # update A matrix
+            for u in range(self.n_dim):
+                for v in range(self.n_dim):
+                    self._update_A(u, v)
+
+            # # check sum of P
+            # self.check_P(t_indices)
+            # # get retrieval test results
+            # n_alerts, precision, recall = self.retrieval_test(t_indices, specific_labels=specific_labels, first_N=first_N)
+            # loglik = self.log_likelihood(T, tau)
+            # lowerb = self.jensens_lower_bound(T, tau)
+            # # logging
+            # print('[%s] iter %d\tlog likli: %f,\tlower bound: %f' % (arrow.now(), e+1, loglik, lowerb))
+            # print('[%s] \t\tnum of alerts: %d,\tprecision: %f,\trecall: %f,\tF-1 score: %f.' % \
+            #     (arrow.now(), n_alerts, precision, recall, F_1(precision, recall)))
+            # precisions.append(precision)
+            # recalls.append(recall)
+            # logliks.append(loglik)
+            # lowerbs.append(lowerb)
+        # return precisions, recalls, logliks, lowerbs
 
     def save(self, path):
         np.savetxt(path + "P.txt", self.P, delimiter=',')
         np.savetxt(path + "A.txt", self.A, delimiter=',')
         np.savetxt(path + "Mu.txt", self.Mu, delimiter=',')
+    
 
-    def check_P(self, t_indices):
-        test = [ self.P[i][j]
-            for i in t_indices
-            for j in t_indices[t_indices<i] ]
-        print('[%s] sum of Pij is %f' % (arrow.now(), sum(test)))
-        # for i in t_indices[0:10]:
-        #     test = [ self.P[i][j] for j in t_indices[t_indices<i] ]
-        #     print('[%s] sum of Pij is %f' % (arrow.now(), sum(test)))
 
 if __name__ == '__main__':
     # generate synthetic data
     # - configuration
     T       = 1.
     n_dim   = 3
-    n_point = 10
+    n_point = 5
     # - data generation
-    seq       = np.random.uniform(low=0., high=T, size=(n_point, 3))
+    seq       = np.random.uniform(low=0., high=T, size=(n_point, 10))
     t_order   = seq[:, 0].argsort()
     seq       = seq[t_order, :]
     seq_s     = np.random.choice(n_dim, n_point)
@@ -292,6 +252,7 @@ if __name__ == '__main__':
     print(seq)
     # model unittest
     hawkes = VecMarkedMultivarHawkes(n_dim=n_dim, T=T, seq=seq)
-    print(hawkes.P)
-    print(hawkes.A)
-    print(hawkes.Mu)
+    print(hawkes.P.sum(axis=1))
+    # print(hawkes.A)
+    # print(hawkes.Mu)
+    hawkes.em_fit()
